a5bf358753fd837b7d58d94c25a2a1f66bdd4df1
Team-Spec-Ops <42931119+Team-Spec-Ops@users.noreply.github.com>
2018-09-04 22:12:26 +0530

    Changes made

--- script/page.js
+++ script/page.js
@@ -1,4 +1,11 @@
-var x=prompt("Give Input for x");
-var element= document.getElementsByTagName("p");
-if(x=="4") 
-	element[0].innerHTML="aaa";
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCOHldHNz4lIgRQ2Rn_kGlHFAs8S2T4r5c",
+    authDomain: "e-library-website.firebaseapp.com",
+    databaseURL: "https://e-library-website.firebaseio.com",
+    projectId: "e-library-website",
+    storageBucket: "e-library-website.appspot.com",
+    messagingSenderId: "1042701291539"
+  };
+  firebase.initializeApp(config);
+  
