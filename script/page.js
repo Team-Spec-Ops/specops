c28e2d1c4b76506b0966aff1e8c287225e5c7d3f
Team-Spec-Ops <42931119+Team-Spec-Ops@users.noreply.github.com>
2018-09-05 02:53:02 +0530

    c

--- script/page.js
+++ script/page.js
@@ -52,7 +52,7 @@ function createAccount()
 {
   var username = document.getElementById("Input_Username_create").value;
   var password = document.getElementById("Input_Password_create").value;
-  firebase.auth().createUserWithEmailAndPassword(username, password).then(Update_user_information();).catch(function(error) {
+  firebase.auth().createUserWithEmailAndPassword(username, password).then(Update_user_information()).catch(function(error) {
   var errorCode = error.code;
   var errorMessage = error.message;
   window.alert("An unexpected error occured" + errorMessage);
